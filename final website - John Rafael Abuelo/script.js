3164a5f2796ccd3c74814a8b06e723de5a8daf9d
JohnRafaelAbuelo <johnrafael1217@gmail.com>
2023-06-06 00:30:30 +0800

    uploading multi-device app files

---
+++ final website - John Rafael Abuelo/script.js
@@ -0,0 +1,4 @@
+function toggleMenu() { /*creating function for menu bar dropdown when user clicks on menu icon*/
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+  }
